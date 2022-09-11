7acaccadfc6efbd72938a1cbd19f84df3c514077
Anthony Gladden <a.gladden360@outlook.com>
2022-09-11 10:40:40 -0700

    wrote second test and function

--- lib/WordAnagrams.rb
+++ lib/WordAnagrams.rb
@@ -1,1 +1,14 @@
 require 'pry'
+
+class Words
+  attr_accessor(:string1, :string2)
+  def initialize(string1,string2)
+    @string1 = split(string1)
+    @string2 = split(string2)
+  end
+
+  def split(x)
+    x.delete('').downcase.gsub(/[!@#$%^&*()-=+|;':",.<>?']/, '').split(//).sort
+  end
+  
+end
--- spec/WordAnagrams_spec.rb
+++ spec/WordAnagrams_spec.rb
@@ -1,11 +1,24 @@
 require 'rspec'
 require 'WordAnagrams'
 
-describe(Word) do 
-  describe('#initialized') do 
+describe(Words) do 
+  describe('#initialize') do 
   it('it will recognize string1 and string2 as attributes') do
-    anagram Words.new('ruby', 'bury')
+    anagram = Words.new('ruby', 'bury')
     expect(anagram.string1).to(eq(['b','r','u','y']))
     expect(anagram.string2).to(eq(['b','r','u','y']))
   end
 end
+
+describe('#split') do 
+  it ('will use a long chain of methods to change the given string into an alphabetical array with no spaces or punctuation and is lowercase') do
+    object = Words.new('is a sentence!', 'ruby')
+    expect(object.string1).to(eq(["a", "c", "e", "e", "e", "i", "n", "n", "s", "s", "t"]))
+  end
+end
+
+
+
+
+end
+
